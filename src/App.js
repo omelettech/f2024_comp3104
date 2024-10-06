421eaf4a97b69dece0077889204ce6971f01f72d
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-10-06 13:52:51 -0400

    Added random image functionality

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import './App.css';
 import React, { useState } from "react";
 import StudentForm from "./components/StudentForm";
 import StudentList from "./components/StudentList";
+import img1 from "./img/1.png"
+import img2 from "./img/2.png"
+import img3 from "./img/3.png"
+const files = [img1, img2, img3]
+ const getImageSource = () => {
+        let rand = Math.floor(Math.random() * files.length);
+        console.log(rand)
+        return files[rand]
+    }
 
 const App = () => {
   const [students, setStudents] = useState([]);
 
   const addStudent = (student) => {
+      student.img = getImageSource()
     setStudents([...students, student]);
   };
 
--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -1,13 +1,22 @@
 import React from "react";
 
-const StudentCard = ({ student }) => {
-  return (
-    <div className="student-card">
-      <h3>{student.name}</h3>
-      <p>Age: {student.age}</p>
-      <p>Major: {student.major}</p>
-    </div>
-  );
+
+
+const StudentCard = ({student}) => {
+
+    return (
+        <div className="student-card">
+            <div style={{display: "flex", flexDirection: "row"}}>
+                <img src={student.img} style={{maxWidth: 200, maxHeight: 200}}/>
+                <div style={{alignContent:"center"}}>
+                    <h3>{student.name}</h3>
+                    <p>Age: {student.age}</p>
+                    <p>Major: {student.major}</p>
+                </div>
+
+            </div>
+        </div>
+    );
 };
 
 export default StudentCard;
