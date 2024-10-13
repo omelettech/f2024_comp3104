f480e80ca12ba95fa24ed222cb9e194d0ef209a4
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-10-13 14:21:07 -0400

    Added alt to img

--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -7,7 +7,7 @@ const StudentCard = ({student}) => {
     return (
         <div className="student-card">
             <div style={{display: "flex", flexDirection: "row"}}>
-                <img src={student.img} style={{maxWidth: 200, maxHeight: 200}}/>
+                <img src={student.img} style={{maxWidth: 200, maxHeight: 200}} alt={"Random images"}/>
                 <div style={{alignContent:"center"}}>
                     <h3>{student.name}</h3>
                     <p>Age: {student.age}</p>
